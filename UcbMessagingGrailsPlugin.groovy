f75594bdcf1eef07edd29452440a3c68508f4e74
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-08-21 08:58:39 -0700

    Convert to Gradle

--- UcbMessagingGrailsPlugin.groovy
+++ UcbMessagingGrailsPlugin.groovy
@@ -1,8 +1,9 @@
 class UcbMessagingGrailsPlugin {
     def group = "edu.berkeley.calnet.plugins"
+    //def group = "edu.berkeley.calnet.grails.plugins"
 
     // the plugin version
-    def version = "0.5-SNAPSHOT"
+    def version = "0.6-SNAPSHOT" // !!! Change in build.gradle too
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
@@ -19,53 +20,43 @@ Utility classes for message queues/topics and for processing with Camel.
 '''
 
     // URL to the plugin's documentation
-    //def documentation = "http://grails.org/plugin/ucb-messaging"
+    def documentation = "https://github.com/calnet-oss/grails-ucb-messaging-plugin/"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "BSD"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [name: "University of California, Berkeley", url: "http://www.berkeley.edu/"]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [system: "GitHub", url: "https://github.com/calnet-oss/grails-ucb-messaging-plugin/issues"]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [url: "https://github.com/calnet-oss/grails-ucb-messaging-plugin"]
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
     }
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
     }
 
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
     }
 
     def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 
     def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
